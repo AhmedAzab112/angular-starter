50e73850a954805ce6d0a1d945d2983db7727fca
ahmedazab <ahmedazab1112@gmail.com>
2023-03-25 20:50:33 +0200

    .

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -10,12 +10,12 @@ import { ModalComponent } from 'src/shared/modal/modal.component';
 export class PortfolioComponent implements OnInit {
 
   readonly imgList: any[] = [
-    {src :'../assets/img/cabin.png', name: 'LOG CABIN'},
-    {src :'../assets/img/cake.png', name: 'TASTY CAKE'},
-    {src :'../assets/img/circus.png', name: 'CIRCUS TENT'},
-    {src :'../assets/img/game.png', name: 'CONTROLLER'},
-    {src :'../assets/img/safe.png', name: 'LOCKED SAFE'},
-    {src :'../assets/img/submarine.png', name: 'SUBMARINE'}
+    {src :'./assets/img/cabin.png', name: 'LOG CABIN'},
+    {src :'./assets/img/cake.png', name: 'TASTY CAKE'},
+    {src :'./assets/img/circus.png', name: 'CIRCUS TENT'},
+    {src :'./assets/img/game.png', name: 'CONTROLLER'},
+    {src :'./assets/img/safe.png', name: 'LOCKED SAFE'},
+    {src :'./assets/img/submarine.png', name: 'SUBMARINE'}
   ];
   constructor(public dialog: MatDialog) {}
   ngOnInit(): void {
